35b9a82aef9292ef0f0fb3ec4248ed84d36846f1
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-06-06 20:38:36 +0300

    Feat: deploment to vercel

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 require('dotenv').config();
 require('express-async-errors');
 
@@ -25,9 +27,9 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // middlewares
 app.use(express.json());
-app.use(express.static('./public/'));
+app.use(express.static(path.resolve(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+app.get('/api/v1', (req, res) => {
     res.send('<h1>File Upload Starter</h1>');
 });
 
@@ -40,9 +42,13 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
+connectDB(process.env.MONGO_URI)
+    .then(console.log('Connected to DB...'))
+    .catch((err) => console.log(err));
+
 const start = async () => {
     try {
-        await connectDB(process.env.MONGO_URI);
+        // await connectDB(process.env.MONGO_URI);
 
         app.listen(port, () => console.log(`Server is listening on port ${port}...`));
     } catch (error) {
@@ -51,3 +57,5 @@ const start = async () => {
 };
 
 start();
+
+module.exports = app;
