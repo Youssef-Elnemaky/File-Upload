adf7c14267a2e15e9ac3d69054d4cd97c467dbb0
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-06-06 16:42:09 +0300

    "feat: include product router in app.js"

--- app.js
+++ app.js
@@ -7,13 +7,23 @@ const app = express();
 // database
 const connectDB = require('./db/connect');
 
+// router
+const productRouter = require('./routes/productRoutes');
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+// middlewares
+app.use(express.json());
+app.use(express.static('./public/'));
+
 app.get('/', (req, res) => {
-  res.send('<h1>File Upload Starter</h1>');
+    res.send('<h1>File Upload Starter</h1>');
 });
+
+// routes
+app.use('/api/v1/products', productRouter);
 
 // middleware
 app.use(notFoundMiddleware);
@@ -22,15 +32,13 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
+    try {
+        await connectDB(process.env.MONGO_URI);
 
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+        app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 start();
